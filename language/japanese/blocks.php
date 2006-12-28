ac672ab3aec58f9eaa1278ce2deeed480ab8b966
srmcatee <srmcatee@9b3c414f-5b12-0410-97f9-d54092e68bf8>
2006-12-28 16:48:08 +0000

    Japanese Checkin.

---
+++ language/japanese/blocks.php
@@ -0,0 +1,6 @@
+<?php
+/* Japanese provided by Paul Donald (UK), 2006 */
+// Blocks
+define("_oscmem_nav_block_title","OSC会員ナビ・ブロック");
+//
+?>
---
+++ language/japanese/main.php
@@ -0,0 +1,3 @@
+<?php
+/* Japanese provided by Paul Donald (UK), 2006 */
+?>
