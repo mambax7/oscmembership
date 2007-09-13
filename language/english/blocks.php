997cb3ef3066092287d78d0c4accf0254c3aaa02
srmcatee <srmcatee@9b3c414f-5b12-0410-97f9-d54092e68bf8>
2007-09-13 16:01:34 +0000

    Fixed Block phrase error lookup.  New problem in latest xoops release.

--- language/english/blocks.php
+++ language/english/blocks.php
@@ -1,5 +1,12 @@
 <?php
 // Blocks
 define("_oscmem_nav_block_title","OSC Membership Navigation Block");
+define("_oscmembership_viewperson","Members");
+define("_oscmembership_viewfamily","Family");
+define("_oscmembership_viewgroup","Groups");
+define("_oscmem_view_cart","View Cart");
+define("_oscmem_nav_reports","Reports/Data");
+
+
 //
 ?>
