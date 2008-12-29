8195eaedd64f1ff85500bf3c8f31819f20bf0be3
srmcatee <srmcatee@9b3c414f-5b12-0410-97f9-d54092e68bf8>
2008-12-29 13:12:13 +0000

    Add Simple Chinese UTF8 language files.

---
+++ language/schinese_utf8/admin.php
@@ -0,0 +1,106 @@
+﻿<?php
+define("_oscmembership_ADMINTITLE","OSC 成员 - 管理");
+//
+
+
+//
+define("_oscmembership_DB_CREATED","表创建成功!");
+/* FILEMANAGEMENT*/
+define("_oscmembership_NUM_SOURCE","源文件数目");
+define("_oscmembership_SUB_SOURCE","提交数量");
+define("_oscmembership_SEL_SOURCE","选择源文件");
+define("_oscmembership_OLD_SOURCE","已经存在的源文件");
+define("_oscmembership_NUM_IMAGE","图像文件数目");
+define("_oscmembership_SUB_IMAGE","提交数量");
+define("_oscmembership_SEL_IMAGE","选择图像文件");
+define("_oscmembership_OLD_IMAGE","已经存在的图像文件");
+define("_oscmembership_SOURCE","已存在的源文件");
+define("_oscmembership_IMAGE","已存在的图像文件");
+define("_oscmembership_SUCC","已上传");
+define("_oscmembership_SIZE","大小");
+define("_oscmembership_DEL","删除文件");
+define("_oscmembership_UP","上传");
+define("_oscmembership_FILE_UP","文件已上传");
+define("_oscmembership_FILES_UP","文件上传完成");
+define("_oscmembership_DEL_FILE_OK","文件删除完成");
+define("_oscmembership_DEL_FILE_ERROR","删除文件出错");
+define ("_oscmembership_WARN","警告：文件将从服务器中删除！！！");
+define("_oscmembership_TITLE_SOURCE","上传HTML或PHP文件 ");
+define("_oscmembership_TITLE_IMAGE","上传图像文件");
+define("_oscmembership_TITLE_DEL","删除文件");
+/*MAINMENU*/
+define("_oscmembership_MM_MMSETT","主菜单设置");
+define("_oscmembership_MM_MMQ","主菜单进入？");
+define("_oscmembership_MM_QYES","是");
+define("_oscmembership_MM_QNO","否");
+define("_oscmembership_MM_WEIGHT","宽(0=隐藏)");
+define("_oscmembership_MM_SUBMIT","提交");
+
+define("_oscmem_noitems","没有可显示的项目");
+
+define("_oscmem_addfamilyrole","添加家庭角色");
+define("_oscmem_addmemberclassification","添加成员分级");
+define("_oscmem_addgrouptype","添加组类型");
+
+define("_oscmem_createsuccess","创建条目成功.");
+
+define("_oscmem_familyrole_optionname","姓名");
+define("_oscmem_option_sort","排序");
+define("_oscmem_itemname","条目名");
+define("_oscmem_itemsequence","序列");
+
+//3
+define("_oscmem_osclist_TITLE_grouptypes","组类型");
+
+//2
+define("_oscmem_osclist_TITLE_familyroles","家庭角色");
+
+//4
+define("_oscmem_osclist_TITLE_memberclassifications","成员分级");
+
+//define("_oscmem_admin_osclist_permissions","Permissions");
+
+//define("_oscmem_admin_osclist_familyroles","Family Roles");
+
+//define("_oscmem_admin_osclist_memberclassification","Member Classifications");
+
+
+//define("_oscmem_admin_osclist_grouptype","Group Types");
+
+define("_oscmem_permissions_view","访问权限");
+define("_oscmem_permissions_view_desc","允许所选组查看成员数据.");
+
+define("_oscmem_permissions_modify","修改权限");
+define("_oscmem_permissions_modify_desc","允许所选组修改成员数据.");
+
+define("_oscmem_permissions_admin","管理权限");
+define("_oscmem_permissions_admin_desc","所选组得到全部权限.");
+
+define("_oscmem_edit","编辑");
+define("_oscmem_admin_access_denied","拒绝访问");
+
+define("_oscmem_admin_create","创建");
+define("_oscmem_admin_save","保存");
+define("_oscmem_admin_UPDATESUCCESS","OSC 成员 数据库更新成功！");
+define("_osmem_admin_createcustomfield_success","自定义范围创建成功！");
+
+//customfield
+define("_oscmem_customfieldOrder","次序");
+define("_oscmem_customName","显示姓名");
+
+define("_oscmem_customfield","个人自定义领域");
+define("_oscmem_customfieldName", "领域名");
+define("_oscmem_customfieldType","类型");
+
+define("_oscmem_customfieldcreate_ack","重定向到创建视图");
+define("_oscmem_customfield_Type","自定义领域类型");
+
+define("_osmem_createcustomfield_success","自定义领域类型创建成功");
+
+
+define("_oscmem_churchname","教堂名");
+
+define("_oscmem_setting_TITLE","教堂详细信息");
+define("_oscmem_website","网址");
+define("_oscmem_addcustomfield","添加自定义领域");
+?>
---
+++ language/schinese_utf8/blocks.php
@@ -0,0 +1,23 @@
+﻿<?php
+// Blocks
+define("_oscmem_nav_block_title","OSC 成员 导航块");
+define("_oscmembership_viewperson_menu","成员");
+define("_oscmembership_viewfamily_menu","家族");
+define("_oscmembership_viewgroup_menu","组");
+define("_oscmem_view_cart_menu","查看清单");
+define("_oscmem_nav_reports_menu","报告/日期");
+
+define("_oscmem_bday_1","一月生日");
+define("_oscmem_bday_2","二月生日");
+define("_oscmem_bday_3","三月生日");
+define("_oscmem_bday_4","四月生日");
+define("_oscmem_bday_5","五月生日");
+define("_oscmem_bday_6","六月生日");
+define("_oscmem_bday_7","七月生日");
+define("_oscmem_bday_8","八月生日");
+define("_oscmem_bday_9","九月生日");
+define("_oscmem_bday_10","十月生日");
+define("_oscmem_bday_11","十一月生日");
+define("_oscmem_bday_12","十二月生日");
+//
+?>
