873bf345c81e7f5083777eb0b66346cf3ef261da
skenow <skenow@9b3c414f-5b12-0410-97f9-d54092e68bf8>
2009-05-14 22:59:30 +0000

    Moving branches folder to the root of the repository and out of the oscMembership trunk

--- branches/oscMembership_6.2_revised/sql/mysql_upgrade_3_15.sql
+++
@@ -1,4 +0,0 @@
-
-ALTER TABLE <dbname>.`<dbprefix>_oscmembership_family` ADD COLUMN `picloc` TEXT  AFTER `editedby`;
-
-ALTER TABLE <dbname>.`<dbprefix>_oscmembership_person` ADD COLUMN `picloc` TEXT  AFTER `editedby`;
